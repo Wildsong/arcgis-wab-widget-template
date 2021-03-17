eac26df5329b96b7ff23e4bb11ada514248ac0b7
bwilson <bwilson@co.clatsop.or.us>
2021-03-17 15:10:06 -0700

    added settings support

---
+++ setting/nls/strings.js
@@ -0,0 +1,8 @@
+define({
+  root: ({
+    instruction: "Create the content that displays in this widget.",
+    defaultContent: "Add text, links, and small graphics here.",
+    productVersion: "Product version: ",
+    kernelVersion: "Kernel version: "
+  })
+});
